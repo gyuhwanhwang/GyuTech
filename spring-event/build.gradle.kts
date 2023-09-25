f416030f1e6a4869aa01db553c78e0d4780d1e68
gyuhwanhwang <ghh@kakao.com>
2023-09-25 21:43:58 +0900

    Feat: spring-event -> 도메인 모듈 의존성 추가

--- spring-event/build.gradle.kts
+++ spring-event/build.gradle.kts
@@ -1,4 +1,6 @@
 dependencies {
+    implementation(project(":domain"))
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     runtimeOnly("com.h2database:h2")
